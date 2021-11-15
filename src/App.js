745366f7f3097b1158df6a048ffe8481e79e886f
varnahan <chanzhuyuan82@gmail.com>
2021-11-15 22:32:38 +0800

    added auth V3

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
-
 function App() {
   return (
     <div className="App">
